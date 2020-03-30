9e7df0fd44dd4b507872c69e829400ed5d47f591
sasie59 <sasie59@gmail.com>
2020-03-30 21:45:27 +0800

    change to react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque laborum facilis, quia reiciendis neque explicabo. Tempore esse quos, sint deserunt, adipisci earum iste obcaecati odit provident numquam vero officia voluptas!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
